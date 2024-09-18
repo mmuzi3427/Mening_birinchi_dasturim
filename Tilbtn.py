8d5181f2e810cd131a662b1214b1965c905ad964
PyDev <mmuzi3427@gmail.com>
2024-09-18 06:07:58 +0500

    Update Tilbtn.py

--- Tilbtn.py
+++ Tilbtn.py
@@ -1,1 +1,13 @@
-
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+def ru():
+    kirish = ReplyKeyboardMarkup(resize_keyboard=True)
+    kirish.add(KeyboardButton("Калькулятор"), KeyboardButton("Случайное число"))
+    return kirish
+def rudel():
+    b = InlineKeyboardMarkup()
+    b.add(InlineKeyboardButton("✖️ Удалить", callback_data="delru"))
+    return b
+def tu():
+    n = InlineKeyboardMarkup()
+    n.add(InlineKeyboardButton("♻️ Я понимаю ️✅", callback_data="delru"))
+    return n
